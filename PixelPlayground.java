7649e880fbc0e6008875b6c21081f4f2300c8b14
pxlpkr <pixelparkour@parkkila.net>
2023-03-07 13:25:17 -0800

    zero blue

--- PixelPlayground.java
+++ PixelPlayground.java
@@ -3,8 +3,17 @@ import java.awt.Color;
 public class PixelPlayground {
 
     /* Removes the blue from every pixel in a given picture. */
-    public static void zeroBlue(Picture pic) {
+    public static Picture zeroBlue(Picture pic) {
+        Picture newPicture = new Picture(pic.getHeight(), pic.getWidth());
+        newPicture.copy(pic, 0, 0);
+        for (int x = 0; x < newPicture.getWidth(); x++) {
+            for (int y = 0; y < newPicture.getHeight(); y++) {
+                Pixel parkour = newPicture.getPixel(x, y);
+                parkour.setBlue(0);
+            }
+        }
 
+        return newPicture;
     }
 
     public static void main(String[] args) {
